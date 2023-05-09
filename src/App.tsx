3879f951f825673dfae2771057109cbd65061f4a
helloyoogs <helloyoogs@naver.com>
2023-05-09 14:03:30 +0900

    build

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,19 @@
 import React from 'react';
 import './style/reset.css';
 import './App.css';
-import {Routes, Route, BrowserRouter} from 'react-router-dom'
+import {Routes, Route, HashRouter} from 'react-router-dom'
 import MainComponent from "./component/MainComponent";
 
 const App = ()=> {
   return (
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <HashRouter basename={process.env.PUBLIC_URL}>
       <div className="App">
         <Routes>
           <Route path='/' element={<MainComponent />} />
 
         </Routes>
       </div>
-      </BrowserRouter>
+      </HashRouter>
   );
 }
 
