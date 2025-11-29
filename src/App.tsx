4f7c5e3e16388ea0d0f82adda229fe46527b5ca1
helloyoogs <helloyoogs@naver.com>
2025-11-29 21:02:44 +0900

    다국어 한국어 파일 작성

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import './App.css';
 import { Routes, Route } from "react-router-dom";
 import MainComponent from "./component/MainComponent";
 import { useTranslation } from "react-i18next";
-localStorage.removeItem("i18nextLng");
 
 const App = () => {
     const { t } = useTranslation();
--- src/component/MainComponent.tsx
+++ src/component/MainComponent.tsx
@@ -1,43 +1,61 @@
 /*eslint-disable*/
-import React, { useRef, useState } from 'react';
+import React, {useState} from 'react';
 import './MainComponent.css';
 import velog from '../img/velog_logo_icon.png';
 import github from '../img/github_icon_white.png';
-import IntroComponent from './IntroComponent';
 import ProjectComponent from './ProjectComponent';
 import CareerComponent from './CareerComponent';
+import i18n from "i18next";
 
+type LangCode = "ko" | "ja";
 
 const MainComponent = () => {
     const [isOpen, setIsOpen]: any = useState(null);
     const handleShowPopup = (openNumber: any) => () => {
         return setIsOpen(openNumber);
     }
+    const changeLanguage = (lang:LangCode) => {
+        i18n.changeLanguage(lang);
+    };
     return (
         <div className="main_component">
             <video muted autoPlay loop className={'bg_video pc'}>
-                <source src={process.env.PUBLIC_URL + "/video/nj_bg2023_24fps.mp4"} type="video/mp4" />
+                <source src={process.env.PUBLIC_URL + "/video/nj_bg2023_24fps.mp4"} type="video/mp4"/>
             </video>
             <video muted autoPlay loop className={'bg_video mobile'}>
-                <source src={process.env.PUBLIC_URL + "/video/nj_bg2023_24fps_mobile.mp4"} type="video/mp4" />
+                <source src={process.env.PUBLIC_URL + "/video/nj_bg2023_24fps_mobile.mp4"} type="video/mp4"/>
             </video>
             <div className={'main_wrap_box'}>
-                <div className={'site_title_box'}>
-                    <p className={'site_title'}>HEE YOUNG</p>
-                    <a href={"https://github.com/helloyoogs?tab=repositories"} target={"_blank"}><img
-                        src={github}
-                        alt={"github_icon"}/></a>
-                    <a href={"https://velog.io/@helloyoogs/series/%ED%9A%8C%EA%B3%A0%EB%A1%9D"} target={"_blank"}> <img
-                        src={velog}
-                        alt={"velog_icon"}/></a>
-                    <a className={"lang_select"}>KR</a>
-                    <a className={"lang_select"}>JP</a>
+                <div className={'site_title_container'}>
+                    <div className={'site_title_box'}>
+                        <p className={'site_title'}>HEE YOUNG</p>
+                        <a href={"https://github.com/helloyoogs?tab=repositories"} target={"_blank"}><img
+                            src={github}
+                            alt={"github_icon"}/></a>
+                        <a href={"https://velog.io/@helloyoogs/series/%ED%9A%8C%EA%B3%A0%EB%A1%9D"} target={"_blank"}>
+                            <img
+                                src={velog}
+                                alt={"velog_icon"}/></a>
+                    </div>
+                    <div className={"site_title_box"}>
+                        {i18n.language === "ja"?
+                        <button
+                            className={"lang_select"}
+                            onClick={() => changeLanguage("ko")}
+                        >
+                            KR
+                        </button>
+                        :
+                        <button
+                            className={"lang_select"}
+                            onClick={() => changeLanguage("ja")}
+                        >
+                            JP
+                        </button>
+                        }
+                    </div>
                 </div>
                 <div className={'folder_wrap'}>
-                <div className={'folder'} onClick={handleShowPopup(1)}>
-                        <div className={'folder_img'}></div>
-                        <p>INTRO</p> {/* 소개 */}
-                    </div>
                     <div className={'folder'} onClick={handleShowPopup(2)}>
                         <div className={'folder_img'}></div>
                         <p>PROJECT</p> {/* 프로젝트 */}
@@ -47,12 +65,11 @@ const MainComponent = () => {
                         <p>CAREER</p>  {/* 경력 */}
                     </div>
                 </div>
-                {/* INTRO */}
-                <IntroComponent isOpen={isOpen} setIsOpen={setIsOpen} />
+
                 {/* PROJECT */}
-                <ProjectComponent isOpen={isOpen} setIsOpen={setIsOpen} />
+                <ProjectComponent isOpen={isOpen} setIsOpen={setIsOpen}/>
                 {/* CAREER */}
-                <CareerComponent isOpen={isOpen} setIsOpen={setIsOpen} />
+                <CareerComponent isOpen={isOpen} setIsOpen={setIsOpen}/>
             </div>
         </div>
     )
